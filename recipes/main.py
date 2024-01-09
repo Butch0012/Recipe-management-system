2d18f5f3b57de71d7949c2e774a925b22dce731c
butch0012 <elnathanmburu@gmail.com>
2024-01-09 16:32:52 +0300

    function creation for recipe

--- recipes/main.py
+++ recipes/main.py
@@ -5,3 +5,10 @@ from recipes.models import Recipe, session
 def cli():
     """Main command group for the recipe management CLI."""
     pass
+
+@cli.command()
+@click.argument('name')
+@click.argument('ingredients')
+@click.argument('instructions')
+def add(name, ingredients, instructions):
+    """Add a new recipe."""
