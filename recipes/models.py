4d3eecd02ce68b60cf54ad88bfdaf01a6be45328
butch0012 <elnathanmburu@gmail.com>
2024-01-09 16:06:51 +0300

    setting up the SQLAlchemy

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+# SQLAlchemy setup
+Base = declarative_base()
+engine = create_engine('sqlite:///recipes.db', echo=False)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
