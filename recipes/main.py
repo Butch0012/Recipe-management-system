88b3e13a07f7894c991a14779bbcda3f23e63b26
butch0012 <elnathanmburu@gmail.com>
2024-01-09 16:34:37 +0300

    commit the changes

--- recipes/main.py
+++ recipes/main.py
@@ -14,3 +14,4 @@ def add(name, ingredients, instructions):
     """Add a new recipe."""
     new_recipe = Recipe(name=name, ingredients=ingredients, instructions=instructions)
     session.add(new_recipe)
+    session.commit()
